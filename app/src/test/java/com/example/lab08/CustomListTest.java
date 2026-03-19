59c7e79262bfc2b962607a267dd2d30123593b47
Zehra121 <27100407@lums.edu.pk>
2026-03-19 05:55:05 +0500

    RED: Failed testHasCity()

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,12 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
-
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        // This will fail initially because hasCity() doesn't exist
+        assertTrue(list.hasCity(calgary));
+    }
 }
