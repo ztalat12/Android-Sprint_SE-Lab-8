4cdf558823f39e58545fd22a7e87806686a2ad9e
Zehra121 <27100407@lums.edu.pk>
2026-03-19 06:03:43 +0500

    RED: Failed testDeleteCity() - ❌ failing build (RED phase)

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -11,4 +11,15 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+
+        list.addCity(calgary);
+        list.deleteCity(calgary);
+
+        assertFalse(list.hasCity(calgary));
+    }
 }
