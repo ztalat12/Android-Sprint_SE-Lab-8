0c7a3b888205d3e0390f30cb587a016a944d7d68
Zehra121 <27100407@lums.edu.pk>
2026-03-19 05:58:00 +0500

    GREEN: Passed testHasCity()

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -18,4 +18,7 @@ public class CustomList {
 
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
 }
